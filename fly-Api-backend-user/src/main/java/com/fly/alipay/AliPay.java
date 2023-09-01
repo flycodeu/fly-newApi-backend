99cea6140358e97b81aa338532ccef9647decdeb
fly <1785613048@qq.com>
2023-09-01 16:29:55 +0800

    first

---
+++ fly-Api-backend-user/src/main/java/com/fly/alipay/AliPay.java
@@ -0,0 +1,11 @@
+package com.fly.alipay;
+
+import lombok.Data;
+
+@Data
+public class AliPay {
+    private String traceNo;
+    private double totalAmount;
+    private String subject;
+    private String alipayTraceNo;
+}
---
+++ fly-Api-backend-user/src/main/java/com/fly/alipay/AliPayConfig.java
@@ -0,0 +1,15 @@
+package com.fly.alipay;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "alipay")
+public class AliPayConfig {
+    private String appId;
+    private String appPrivateKey;
+    private String alipayPublicKey;
+    private String notifyUrl;
+}
